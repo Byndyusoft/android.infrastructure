e91de501aeaeee17b87b6f7a42a974bca8cb748e
komiakov.dmitrii <komiakov.dmitrii@gmail.com>
2016-10-26 17:23:36 +0500

    some base features

---
+++ app/src/main/java/com/byndyusoft/androidinfrastructure/di/modules/AppModule.java
@@ -0,0 +1,11 @@
+package com.byndyusoft.androidinfrastructure.di.modules;
+
+/**
+ * Created by Dmitrii Komiakov
+ * komiakov.dmitrii@gmail.com
+ * komyakovds@byndyusoft.com
+ * on 26.10.2016.
+ */
+
+public class AppModule {
+}
